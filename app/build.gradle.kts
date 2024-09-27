312b4037f73845b9a69909ad9280f3e682a8a904
NAOfumich <123987123qw@mail.ru>
2024-09-27 14:40:00 +0500

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,14 @@ android {
 
 dependencies {
 
+    //retrofit and coil
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.2.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/ufanet_practice/MainActivity.kt
+++ app/src/main/java/com/example/ufanet_practice/MainActivity.kt
@@ -3,27 +3,62 @@ package com.example.ufanet_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.activity.viewModels
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.ufanet_practice.ui.theme.Ufanet_PracticeTheme
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import androidx.compose.foundation.layout.Column
+import coil.compose.rememberAsyncImagePainter
+import com.example.ufanet_practice.data.StoriesViewModel
+import com.example.ufanet_practice.data.Story
+import com.example.ufanet_practice.ui_component.SearchBarComponent
+
 
 class MainActivity : ComponentActivity() {
+    private val storiesViewModel: StoriesViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Ufanet_PracticeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            StoriesScreen(storiesViewModel)
+        }
+    }
+}
+
+@Composable
+fun StoriesScreen(viewModel: StoriesViewModel) {
+    val stories by viewModel.stories.collectAsState()
+    var searchQuery = remember { mutableStateOf("") }
+
+    Column {
+        // Вызов SearchBarComponent
+        SearchBarComponent(searchText = searchQuery)
+
+        // Отображаем только отфильтрованные истории
+        StoriesGrid(stories = stories.filter { it.newsName.contains(searchQuery.value, ignoreCase = true) })
+    }
+}
+
+@Composable
+fun StoriesGrid(stories: List<Story>) {
+    // LazyColumn для создания вертикального списка
+    LazyColumn {
+        //разбиваю на 2 строки список
+        items(stories.chunked(2)) { pair ->
+            Row(modifier = Modifier.fillMaxWidth()) {
+                pair.forEach { story ->
+                    StoryItem(story)
                 }
             }
         }
@@ -31,17 +66,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun StoryItem(story: Story) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+        // равномерное распределение ширины
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Ufanet_PracticeTheme {
-        Greeting("Android")
+    ) {
+        Column {
+            //Coil для загрузки изображений
+            Image(
+                painter = rememberAsyncImagePainter(story.imageLogo),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(120.dp) // Установите желаемую высоту для изображения
+            )
+            //название истории
+            Text(
+                text = story.newsName,
+                modifier = Modifier.padding(8.dp),
+            )
+        }
     }
 }
