b6c64fd8580095938a2b1f5b7f3bfc5d89e5329b
NAOfumich <123987123qw@mail.ru>
2024-10-14 21:36:23 +0500

    Попробовал юзануть Scaffold

--- app/src/main/java/com/example/ufanet_practice/presentation/ui/StoriesScreen.kt
+++ app/src/main/java/com/example/ufanet_practice/presentation/ui/StoriesScreen.kt
@@ -1,24 +1,33 @@
 package com.example.ufanet_practice.presentation.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.example.ufanet_practice.presentation.viewmodel.FavoritesStoryViewModel
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.padding
 
 @Composable
 fun StoriesScreen(favoritesStoryViewModel: FavoritesStoryViewModel) {
-    // Получаем фильтрованные истории
+    // фильтрованные истории
     val stories by favoritesStoryViewModel.stories.collectAsState()
 
     // Состояние для поиска
     val searchQuery = remember { mutableStateOf("") }
 
-    // UI-компоненты
-    Column {
-        SearchBarComponent(searchText = searchQuery, viewModel = favoritesStoryViewModel)
-        StoriesGrid(stories = stories, favoritesViewModel = favoritesStoryViewModel)
-    }
+    // Scaffold для топбара и контента
+    Scaffold(
+        topBar = {
+            SearchBarComponent(searchText = searchQuery, viewModel = favoritesStoryViewModel)
+        },
+        content = { paddingValues ->
+            Column(modifier = Modifier.padding(paddingValues)) {
+                StoriesGrid(stories = stories, favoritesViewModel = favoritesStoryViewModel)
+            }
+        }
+    )
 }
